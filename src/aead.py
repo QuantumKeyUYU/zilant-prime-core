bfe92f58abc9552ab958a13da96aa203da12ccb1
QuantumKeyUYU <uyu.enterglobal@gmail.com>
2025-06-06 20:59:25 +0300

    Add SPDX headers to legacy utility modules

--- src/aead.py
+++ src/aead.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025 Zilant Prime Core Contributors
+
 from __future__ import annotations
 
 import os
--- src/utils/entropy.py
+++ src/utils/entropy.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025 Zilant Prime Core Contributors
+
 import os
 import time
 
