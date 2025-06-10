da0eb159150e4b216b1e4dcf220551d3e05ba679
QuantumKeyUYU <uyu.enterglobal@gmail.com>
2025-06-10 08:20:12 +0300

    test: fix hooking detection monkeypatch

--- tests/test_root_guard_extra.py
+++ tests/test_root_guard_extra.py
@@ -1,7 +1,11 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2025 Zilant Prime Core contributors
 
+import builtins
+
 import zilant_prime_core.utils.root_guard as rg
+
+original_open = builtins.open
 
 
 def test_all_root_checks(monkeypatch):
@@ -44,9 +48,7 @@ def test_hooking_detection(monkeypatch):
             import io
 
             return io.StringIO("/usr/lib/frida.so")
-        return open(path, mode, *args, **kwargs)
-
-    import builtins
+        return original_open(path, mode, *args, **kwargs)
 
     monkeypatch.setattr(builtins, "open", fake_open)
     assert rg.is_device_rooted() is True
