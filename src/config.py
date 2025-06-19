d2cf774941002caf47b8dfc801d253b48e460a96
QuantumKeyUYU <uyu.enterglobal@gmail.com>
2025-06-19 07:39:23 +0300

    Run isort

--- src/config.py
+++ src/config.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 """Project configuration options."""
 
-import tomllib
 from pathlib import Path
 from typing import Any
+
+import tomllib
 
 ROOT = Path(__file__).resolve().parents[1]
 
